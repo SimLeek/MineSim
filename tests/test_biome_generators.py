6c945f4c93d24b186e6cd07594e1b690d416d0a9
SimLeek <josh.miklos@gmail.com>
2017-11-15 14:51:02 -0700

    added binary tests for biomes

--- tests/test_biome_generators.py
+++ tests/test_biome_generators.py
@@ -5,7 +5,8 @@ import minesim.biome_generators as biogen
 import numpy as np
 
 import vtk
-
+import os
+data_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep + 'data'+os.sep
 
 def convertToNumber (s):
     return int.from_bytes(s.encode(), 'little')
@@ -70,54 +71,49 @@ class TestBiomeGenerators(unittest.TestCase):
     # todo: change this to a non-interactive test, like glsl_compute tests
     # todo: add interactive tests that require y/n == y to pass
     def testFullBiomeGen(self):
-        plains = biogen.plains_gen( 500, 500, 100, 30, 50, turbulence=0.01).astype(np.bool_)
+        plains = biogen.plains_gen( 50, 50, 10, 3, 5, turbulence=0.1).astype(np.bool_)
         min_h = 0
-        max_h = 35
+        max_h = 3
         h = max_h-min_h
-        dissipation = 0.05
+        dissipation = 0.5
         # us, but not them boolean operators to remove boolean arrays from other boolean arrays
         # if I run into long strings of operators like these, consider moving back to glsl.
-        cave_caves = biogen.cloud_layer_gen(500, 500, h, 0.02, 0.0, 0) & ~biogen.cloud_layer_gen(500, 500, h, 0.1, 0.0, 0)
-        cave_caves = biogen.cloud_layer_gen(500, 500, h, 0.005, 0.0, dissipation) & ~cave_caves
-        plains[0:500, 0:500, min_h:max_h] = plains[0:500, 0:500, min_h:max_h] & ~cave_caves
+        cave_caves = biogen.cloud_layer_gen(50, 50, h, 0.2, 0.0, 0) & ~biogen.cloud_layer_gen(50, 50, h, 1, 0.0, 0)
+        cave_caves = biogen.cloud_layer_gen(50, 50, h, 0.05, 0.0, dissipation) & ~cave_caves
+        plains[0:50, 0:50, min_h:max_h] = plains[0:50, 0:50, min_h:max_h] & ~cave_caves
         visible_plains = biogen.restrict_visible(plains, plains, show_bounds=True)
-        visible_locations = biogen.get_locations(visible_plains)
-        mockmod = MockModel()
-        mockmod.batch_blocks(visible_locations, 'grass')
-        mockmod.show_cloud()
+        visible_locations = biogen.get_locations(visible_plains).astype(np.uint16)  # even as 48 bits, should still be less
+        with open(data_dir + self.testFullBiomeGen.__name__ + ".binary", 'rb') as test_file:
+            self.assertEqual(test_file.read(),np.packbits(visible_locations).tobytes())
+
     def testVisibleGen(self):
-        plains = biogen.cloud_layer_gen(100, 100, 50, 0.02, 0.0, 0)
+        plains = biogen.cloud_layer_gen(10, 10, 5, 0.2, 0.0, 0)
         visible_plains = biogen.restrict_visible(plains, plains, show_bounds=True)
-        visible_locations = biogen.get_locations(visible_plains)
-        mockmod = MockModel()
-        mockmod.batch_blocks(visible_locations, 'grass')
-        mockmod.show_cloud()
+        visible_locations = biogen.get_locations(visible_plains).astype(np.uint16)
+        with open(data_dir + self.testVisibleGen.__name__ + ".binary", 'rb') as test_file:
+            self.assertEqual(test_file.read(), np.packbits(visible_locations).tobytes())
 
     def testPlainsGen(self):
-        mockmod = MockModel()
-        mockmod.batch_blocks(
-            biogen.get_locations(
-                biogen.plains_gen( 100, 100, 100, 30, 50, turbulence=0.01)
-            ), 'grass'
-        )
-        mockmod.show_cloud()
+        plains = biogen.plains_gen( 10, 10, 10, 3, 5, turbulence=0.1).astype(np.bool_)
+        with open(data_dir + self.testPlainsGen.__name__ + ".binary", 'rb') as test_file:
+            self.assertEqual(test_file.read(), np.packbits(plains).tobytes())
 
     def testStoneContainer(self):
-        mockmod = MockModel()
-        mockmod.batch_blocks(biogen.get_locations(biogen.stone_container_gen( 100, 100, 100)), 'grass')
-        mockmod.show_cloud()
+        stone = biogen.get_locations(biogen.stone_container_gen( 10, 10, 10))
+        with open(data_dir + self.testStoneContainer.__name__ + ".binary", 'rb') as test_file:
+            self.assertEqual(test_file.read(), np.packbits(stone).tobytes())
 
     def testCloudLayerGen(self):
-        mockmod = MockModel()
-        mockmod.batch_blocks(biogen.get_locations(biogen.cloud_layer_gen(100, 100, 100, 0.02, 0.0, .1)), 'grass')
-        mockmod.show_cloud()
+        clouds = biogen.get_locations(biogen.cloud_layer_gen(10, 10, 10, 0.2, 0.0, .5))
+        with open(data_dir + self.testCloudLayerGen.__name__ + ".binary", 'rb') as test_file:
+            self.assertEqual(test_file.read(), np.packbits(clouds).tobytes())
 
     def testFoamGen(self):
-        mockmod = MockModel()
-        mockmod.batch_blocks(biogen.get_locations(biogen.cloud_layer_gen(100, 100, 100, 0.02, 0.0, 0)), 'grass')
-        mockmod.show_cloud()
+        foam = biogen.get_locations(biogen.cloud_layer_gen(10, 10, 10, 0.2, 0.0, 0))
+        with open(data_dir + self.testFoamGen.__name__ + ".binary", 'rb') as test_file:
+            self.assertEqual(test_file.read(), np.packbits(foam).tobytes())
 
     def testCaveDig(self):
-        mockmod = MockModel()
-        mockmod.batch_blocks(biogen.get_locations(biogen.inverse_cloud_layer_gen(100, 100, 100, 0.02, 0.5, .05)), 'grass')
-        mockmod.show_cloud()
+        caves = biogen.get_locations(biogen.inverse_cloud_layer_gen(10, 10, 10, 0.2, 0.5, .5))
+        with open(data_dir + self.testCaveDig.__name__ + ".binary", 'rb') as test_file:
+            self.assertEqual(test_file.read(), np.packbits(caves).tobytes())
